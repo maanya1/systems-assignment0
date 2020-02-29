b9ac79bac86e8cbdfcfc4ee890f79916799513f5
Sam Olaogun <sam.olaogun@gmail.com>
2020-02-29 16:36:46 -0500

    Formatted code and removed comments.

--- filesort.c
+++ filesort.c
@@ -1,290 +1,280 @@
-#include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-struct Node{
-    char* info;
-    struct Node* next;
+struct Node {
+  char* info;
+  struct Node* next;
 };
 typedef struct Node Node;
 
-Node* makeNode(char* c){
-    Node* byt = malloc(sizeof(Node));
-    byt->info = c;
-    byt->next = NULL;
-    return byt;
+Node* makeNode(char* c) {
+  Node* byt = malloc(sizeof(Node));
+  byt->info = c;
+  byt->next = NULL;
+  return byt;
 }
 
 void printList(Node* list) {
-    Node* curr = list;
+  Node* curr = list;
 
-    while (curr != NULL) {
-        printf("'%s' ",curr->info);
-        curr = curr->next;
+  while (curr != NULL) {
+    printf("'%s' ", curr->info);
+    curr = curr->next;
+  }
+
+  printf("\n");
+}
+
+int isWhitespace(char* c) { return (*c == ' ' || *c == '\n' || *c == '\t'); }
+
+int lenList(Node* head) {
+  Node* curr = head;
+  int len = 0;
+  while (curr != NULL) {
+    len++;
+    curr = curr->next;
+  }
+  return len;
+}
+
+char* listToString(Node* head) {
+  int len = lenList(head);
+  char* string = malloc((sizeof(char) * len) + 1);
+  Node* curr = head;
+  int ind = 0;
+
+  while (curr != NULL) {
+    string[ind] = *(curr->info);
+    curr = curr->next;
+    ind++;
+  }
+
+  string[ind] = '\0';
+  return string;
+}
+
+struct Node* fileRead(int fd) {
+  int byteCount = 0;
+  Node* head = NULL;
+  Node* curr = NULL;
+  Node* miniHead = NULL;
+  Node* miniCurr = NULL;
+  do {
+    char* byt = malloc(sizeof(char));
+    byteCount = read(fd, byt, 1);
+    if (isalpha(*byt) || isdigit(*byt) || *byt == '-') {
+      Node* charNode = makeNode(byt);
+      if (miniHead == NULL) {
+        miniHead = charNode;
+        miniCurr = charNode;
+      } else {
+        miniCurr->next = charNode;
+        miniCurr = miniCurr->next;
+      }
     }
-
-    printf("\n");
-}
-
-int isWhitespace(char* c){
-    return (*c == ' ' || *c == '\n' || *c == '\t');
-}
-
-int lenList(Node* head){
-    Node* curr = head;
-    int len = 0;
-    while(curr != NULL){
-        len++;
-        curr = curr->next;
-    }
-    return len;
-}
-
-char* listToString(Node* head){
-    int len = lenList(head);
-    char* string = malloc((sizeof(char) *len)+1);
-    Node* curr = head;
-    int ind = 0;
-
-    while(curr != NULL){
-        string[ind] = *(curr->info);
-        curr = curr->next;
-        ind++;
-    }
-
-    string[ind] = '\0';
-    return string;
-}
-
-struct Node* fileRead(int fd){
-    int byteCount = 0;
-    Node* head = NULL;
-    Node* curr = NULL;
-    Node* miniHead = NULL;
-    Node* miniCurr = NULL;
-    do{
-        char* byt = malloc(sizeof(char));
-        byteCount = read(fd, byt, 1);
-        // printf("%s\n", byt);
-        if(isalpha(*byt) || isdigit(*byt) || *byt == '-'){
-            Node* charNode = makeNode(byt);
-            if(miniHead == NULL){
-                miniHead = charNode;
-                miniCurr = charNode;
-            } else {
-                miniCurr->next = charNode;
-                miniCurr = miniCurr->next;
-            }
-        }
-        if(*byt == ','){
-            char* string = listToString(miniHead);
-            Node* charNode = makeNode(string);
-            if(head == NULL){
-                head = charNode;
-                curr = charNode;
-            } else {
-                curr->next = charNode;
-                curr = curr->next;
-            }
-            miniHead = NULL;
-            miniCurr = NULL;
-        }
-    } while(byteCount>0);
-     char* string = listToString(miniHead);
-    Node* charNode = makeNode(string);
-    if(head == NULL){
+    if (*byt == ',') {
+      char* string = listToString(miniHead);
+      Node* charNode = makeNode(string);
+      if (head == NULL) {
         head = charNode;
         curr = charNode;
-    } else {
+      } else {
         curr->next = charNode;
         curr = curr->next;
+      }
+      miniHead = NULL;
+      miniCurr = NULL;
     }
+  } while (byteCount > 0);
+  char* string = listToString(miniHead);
+  Node* charNode = makeNode(string);
+  if (head == NULL) {
+    head = charNode;
+    curr = charNode;
+  } else {
+    curr->next = charNode;
+    curr = curr->next;
+  }
 
+  return head;
+}
+
+int stringCompare(void* str1, void* str2) {
+  Node* s1 = str1;
+  Node* s2 = str2;
+
+  return underTheHood(s1->info, s2->info);
+}
+
+int underTheHood(char* str1, char* str2) {
+  if (str1 == NULL && str2 == NULL) {
+    return 0;
+  }
+  if (str1 == NULL) {
+    return *str2;
+  }
+  if (str2 == NULL) {
+    return *str1;
+  }
+
+  while (*str1 != '\0' && *str2 != '\0') {
+    int diff = *str1 - *str2;
+    if (diff != 0) {
+      return diff;
+    }
+    str1++;
+    str2++;
+  }
+
+  if (*str1 == '\0' && *str2 == '\0') {
+    return 0;
+  }
+  if (*str1 == '\0') {
+    return -(*str2);
+  }
+  if (*str2 == '\0') {
+    return *str1;
+  }
+
+  return 0;
+}
+
+Node* appendToList(Node* head, Node* new) {
+  if (head == NULL && new == NULL) {
+    return 0;
+  }
+  if (head == NULL) {
+    return new;
+  } else if (new == NULL) {
     return head;
-}
-
-int stringCompare(void* str1, void* str2){
-    Node* s1 = str1;
-    Node* s2 = str2;
-
-    return underTheHood(s1->info, s2->info);
-}
-
-int underTheHood(char* str1, char* str2){
-    if(str1 == NULL && str2 == NULL){
-        return 0;
-    }
-    if(str1 == NULL){
-        return *str2;
-    }
-    if(str2 == NULL){
-        return *str1;
-    }
-
-    while(*str1 != '\0' && *str2 != '\0'){
-        int diff = *str1-*str2;
-        if(diff != 0){
-            return diff;
-        }
-        str1++;
-        str2++;
-    }
-
-    if(*str1 == '\0' && *str2 == '\0'){
-        return 0;
-    }
-    if(*str1 == '\0'){
-        return -(*str2);
-    }
-    if(*str2 == '\0'){
-        return *str1;
-    }
-
-    return 0;
-}
-
-Node* appendToList(Node* head, Node* new){
-   if (head==NULL && new==NULL){
-       return 0;
-   }
-   if(head == NULL){
-       return new;
-   } else if (new == NULL){
-       return head;
-   } else {
-       Node* curr = head;
-       Node* prev = NULL;
-       while(curr != NULL){
-           prev = curr;
-           curr = curr->next;
-       }
-       prev->next = new;
-   }
-   return head;
-}
-
-int insertionSort(void* toSort, int (*comparator)(void*, void*)){
-    Node* dummyHead = toSort;
-    Node* curr = dummyHead->next;
+  } else {
+    Node* curr = head;
     Node* prev = NULL;
-
-
-    while(curr != NULL){
-        Node* ptr = dummyHead->next;
-        Node* ptrPrev = NULL;
-
-        while(ptr != curr){
-            if(comparator(curr, ptr) < 0){
-                if(ptrPrev == NULL){
-                    prev->next = curr->next;
-                    curr->next = dummyHead->next;
-                    dummyHead->next = curr;
-                } else {
-                    prev->next = curr->next;
-                    curr->next = ptr;
-                    ptrPrev->next = curr;
-                }
-
-                break;
-            }
-
-            ptrPrev = ptr;
-            ptr = ptr->next;
-        }
-      
-        prev = curr;
-        curr = curr->next;
+    while (curr != NULL) {
+      prev = curr;
+      curr = curr->next;
     }
-
-    // printList(toSort);
-    return 0;
+    prev->next = new;
+  }
+  return head;
 }
 
-int quickSort(void* toSort, int (*comparator)(void*, void*)){
-    Node* head = toSort;
+int insertionSort(void* toSort, int (*comparator)(void*, void*)) {
+  Node* dummyHead = toSort;
+  Node* curr = dummyHead->next;
+  Node* prev = NULL;
 
-  
-    Node* pivot = head->next;
-   
+  while (curr != NULL) {
+    Node* ptr = dummyHead->next;
+    Node* ptrPrev = NULL;
 
-    Node* less = NULL;
-    Node* greater = NULL;
-    Node* prev = NULL;
+    while (ptr != curr) {
+      if (comparator(curr, ptr) < 0) {
+        if (ptrPrev == NULL) {
+          prev->next = curr->next;
+          curr->next = dummyHead->next;
+          dummyHead->next = curr;
+        } else {
+          prev->next = curr->next;
+          curr->next = ptr;
+          ptrPrev->next = curr;
+        }
 
-    if(pivot == NULL || pivot->next == NULL){
-        return 0;
-    }
-
-    Node* curr = pivot->next;
-     pivot->next = NULL;
-
-    while(curr != NULL){
-      Node* next = curr->next;
-      curr->next = NULL;
-
-      if(comparator(pivot, curr)>0){ 
-        less = appendToList(less, curr);
-      } else {
-        greater = appendToList(greater, curr);
+        break;
       }
 
-     curr = next;
+      ptrPrev = ptr;
+      ptr = ptr->next;
     }
 
-//   printf("end1\n");
-      
-   Node* dummyNode1 = malloc(sizeof(Node));
-   dummyNode1->next = less;
-  
-   Node* dummyNode2 = malloc(sizeof(Node));
-   dummyNode2->next = greater;
+    prev = curr;
+    curr = curr->next;
+  }
 
-    quickSort(dummyNode1, comparator);
-    quickSort(dummyNode2, comparator);
-    head->next = appendToList(appendToList(dummyNode1->next, pivot),dummyNode2->next);
+  return 0;
+}
+
+int quickSort(void* toSort, int (*comparator)(void*, void*)) {
+  Node* head = toSort;
+
+  Node* pivot = head->next;
+
+  Node* less = NULL;
+  Node* greater = NULL;
+  Node* prev = NULL;
+
+  if (pivot == NULL || pivot->next == NULL) {
     return 0;
-}
+  }
 
-int int_comparator (void* a, void* b){
-    Node* a2 = a;
-    Node* b2 = b;
-    return atoi(a2->info)-atoi(b2->info);
-}
+  Node* curr = pivot->next;
+  pivot->next = NULL;
 
-int listIsString(Node* head){
-    if(head == NULL){
-        return 1;
-    }
-    Node* curr = head;
-    if(*(curr->info) == '\0'){
-        return listIsString(curr->next);
-    }
-    return atoi(curr->info) == 0 && *(curr->info) != '0'; //return 1 if string, 0 if not string
-}
+  while (curr != NULL) {
+    Node* next = curr->next;
+    curr->next = NULL;
 
-int main(int argc, char** argv){
-    int fd = open(argv[2], O_RDONLY | O_CREAT);
-
-    Node* list = fileRead(fd);
-    int isString = listIsString(list);
-    printf("is str: %d\n", isString);
-    printList(list);
-    Node* dummyNode = malloc(sizeof(Node));
-    dummyNode->next = list;
-
-
-    int (*cmpr)(void*, void*) = isString ? stringCompare : int_comparator;
-
-    if(underTheHood(argv[1], "-i") == 0){
-        insertionSort(dummyNode, cmpr);
+    if (comparator(pivot, curr) > 0) {
+      less = appendToList(less, curr);
+    } else {
+      greater = appendToList(greater, curr);
     }
 
-    if(underTheHood(argv[1], "-q") == 0){
-        quickSort(dummyNode, cmpr);
-    }
+    curr = next;
+  }
 
-    printList(dummyNode->next);
-    return 0;
+  Node* dummyNode1 = malloc(sizeof(Node));
+  dummyNode1->next = less;
+
+  Node* dummyNode2 = malloc(sizeof(Node));
+  dummyNode2->next = greater;
+
+  quickSort(dummyNode1, comparator);
+  quickSort(dummyNode2, comparator);
+  head->next =
+      appendToList(appendToList(dummyNode1->next, pivot), dummyNode2->next);
+  return 0;
 }
 
+int int_comparator(void* a, void* b) {
+  Node* a2 = a;
+  Node* b2 = b;
+  return atoi(a2->info) - atoi(b2->info);
+}
+
+int listIsString(Node* head) {
+  if (head == NULL) {
+    return 1;
+  }
+  Node* curr = head;
+  if (*(curr->info) == '\0') {
+    return listIsString(curr->next);
+  }
+  return atoi(curr->info) == 0 &&
+         *(curr->info) != '0';  // return 1 if string, 0 if not string
+}
+
+int main(int argc, char** argv) {
+  int fd = open(argv[2], O_RDONLY | O_CREAT);
+
+  Node* list = fileRead(fd);
+  int isString = listIsString(list);
+
+  Node* dummyNode = malloc(sizeof(Node));
+  dummyNode->next = list;
+
+  int (*cmpr)(void*, void*) = isString ? stringCompare : int_comparator;
+
+  if (underTheHood(argv[1], "-i") == 0) {
+    insertionSort(dummyNode, cmpr);
+  }
+
+  if (underTheHood(argv[1], "-q") == 0) {
+    quickSort(dummyNode, cmpr);
+  }
+
+  printList(dummyNode->next);
+  return 0;
+}
